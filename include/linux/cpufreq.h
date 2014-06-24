3ad2164b38391dda2547a947fc86752b32f473fd
Arnav <nolinuxnoparty@gmail.com>
2014-06-24 15:18:19 -0400

    Revert "nicki: add intellidemand v4.2"
    This reverts commit 39cb5c94f5c271513607aa44c37bb694f53ea37f.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -410,9 +410,6 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
-extern struct cpufreq_governor cpufreq_gov_intellidemand;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 #endif
 
 
